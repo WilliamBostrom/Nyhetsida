12ec001224f0b20411e6de83003d2e2999d10e84
WilliamBostrom <133894488+WilliamBostrom@users.noreply.github.com>
2024-01-16 23:03:56 +0100

    css

--- src/js/utility/utility.js
+++ src/js/utility/utility.js
@@ -6,8 +6,8 @@ const menuIcon = document.querySelector('.icon-mobil-nav[name="menu"]');
 const closeIcon = document.querySelector('.icon-mobil-nav[name="close"]');
 let visible = false;
 
-mobileBtn.addEventListener("click", function (e) {
-  visible = !visible; // Byt värdet mellan true och false vid varje klick
+mobileBtn.addEventListener("click", function () {
+  visible = !visible; // Byter värdet mellan true och false vid varje klick
 
   menuIcon.style.display = visible ? "none" : "block";
   closeIcon.style.display = visible ? "block" : "none";
