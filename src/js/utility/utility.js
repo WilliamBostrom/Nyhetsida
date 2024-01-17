4deeb6983a905a9e7715173c1cac8ea63d44ad5a
WilliamBostrom <133894488+WilliamBostrom@users.noreply.github.com>
2024-01-17 13:59:02 +0100

    side-nav nästan klar

--- src/js/utility/utility.js
+++ src/js/utility/utility.js
@@ -1,14 +1,24 @@
-////
-// Början för få fram extra meny
-
 const mobileBtn = document.querySelector(".btn-mobile-nav");
 const menuIcon = document.querySelector('.icon-mobil-nav[name="menu"]');
 const closeIcon = document.querySelector('.icon-mobil-nav[name="close"]');
+const side_nav = document.querySelector(".side-nav");
+
 let visible = false;
 
 mobileBtn.addEventListener("click", function () {
-  visible = !visible; // Byter värdet mellan true och false vid varje klick
+  visible = !visible; // Toggle the value between true and false on each click
 
   menuIcon.style.display = visible ? "none" : "block";
   closeIcon.style.display = visible ? "block" : "none";
+
+  // Toggle styles for side_nav based on the value of 'visible'
+  if (visible) {
+    side_nav.style.display = "block";
+    side_nav.style.opacity = 1;
+    side_nav.style.visibility = "visible";
+  } else {
+    side_nav.style.display = "none";
+    side_nav.style.opacity = 0;
+    side_nav.style.visibility = "hidden";
+  }
 });
