b5b43d3e02da5db95c1ba0da90e6fd4a7cf6825c
WilliamBostrom <133894488+WilliamBostrom@users.noreply.github.com>
2024-01-18 22:43:49 +0100

    Medlem, logga in och ut klart

--- src/js/utility/login.js
+++ src/js/utility/login.js
@@ -1,4 +1,6 @@
+//////////////////////
 /* Modal bli medlem */
+/////////////////////
 const btnsOpenMember = document.querySelectorAll(".show-members");
 const memberOverlay = document.querySelector(".member-overlay");
 const memberContainer = document.querySelector(".signup-container");
@@ -36,9 +38,91 @@ document.addEventListener("keydown", function (e) {
   }
 });
 
-/* Här ska jag börja bygga på obj som datan från registrerade hamnar */
+/* För att logga in */
+const formSignin = document.getElementById("signin-form"),
+  usernameSignin = document.getElementById("username-signin"),
+  loginPassword = document.getElementById("password-signin"),
+  btnOpenLogin = document.querySelectorAll(".login"),
+  signInOverlay = document.querySelector(".signin-overlay"),
+  signInContainer = document.querySelector(".signin-container");
 
+let isLoggedIn = false;
+let loginContainer = document.querySelector(".login-container");
+
+console.log(loginContainer);
+
+////////////////////
+/* MODAL LOGGA IN */
+////////////////////
+
+btnOpenLogin.forEach(function (btn) {
+  btn.addEventListener("click", function () {
+    openLogin();
+  });
+});
+
+//Stänger logga in
+function closeLogin() {
+  signInContainer.classList.add("hidden-signin");
+  signInOverlay.classList.add("hidden-signin");
+  document.body.classList.remove("modal-open");
+}
+
+// Om man trycker utanför rutan stängs den
+signInOverlay.addEventListener("click", function () {
+  closeLogin();
+});
+
+// Om man trycker Escape stängs login
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !signInContainer.classList.contains("hidden")) {
+    closeMembers();
+  }
+});
+
+// Öppnar bli Logga in
+function openLogin() {
+  signInContainer.classList.remove("hidden-signin");
+  signInOverlay.classList.remove("hidden-signin");
+  document.body.classList.add("modal-open");
+}
+/////////////////////////////
+/* FÖR ATT LOGGA IN OCH UT */
+/////////////////////////////
+
+formSignin.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const storedUserJSON = localStorage.getItem("currentUser");
+
+  if (storedUserJSON) {
+    const storedUser = JSON.parse(storedUserJSON);
+    if (
+      usernameSignin.value === storedUser.name &&
+      loginPassword.value === storedUser.password
+    ) {
+      // Inloggning lyckad
+      isLoggedIn = true;
+      alert(`Välkommen in ${storedUser.name}`);
+      loginContainer.innerHTML = `<a class="main-nav-btn nav-cta logout" href="#cta">Logga ut</a>`;
+      closeLogin();
+      //Logga ut
+      const btnLogOut = document.querySelector(".logout");
+      btnLogOut.addEventListener("click", () => {
+        alert("Du är utloggad");
+        isLoggedIn = false;
+        loginContainer.innerHTML = `<a class="main-nav-btn nav-cta login" href="#cta">Logga in</a>`;
+      });
+    } else {
+      showError(usernameSignin, "Felaktigt användarnamn eller lösenord");
+    }
+  } else {
+    showError(usernameSignin, "Ingen användare hittad");
+  }
+});
+
+/* /////////// */
 /* BLI MEDLEM */
+/* /////////// */
 
 const form = document.getElementById("signup-form"),
   username = document.getElementById("username"),
@@ -57,16 +141,19 @@ form.addEventListener("submit", async (e) => {
   const inputValid = await checkInput();
 
   if (inputValid.every((isValid) => isValid)) {
-    newUser = true;
+    const createdUser = new NewUser(
+      username.value,
+      password.value,
+      email.value
+    );
+
+    const userJSON = JSON.stringify(createdUser);
+
+    localStorage.setItem("currentUser", userJSON);
+
     closeMembers();
-
-    const obj = {
-      username: username.value,
-      email: email.value,
-      password: password.value,
-    };
-
-    createUser(obj);
+    closeMembers();
+    createUser(createdUser);
   }
 });
 
@@ -127,6 +214,26 @@ async function showSuccess(input) {
   formControl.className = "form-control success";
 }
 
-function createUser(obj) {
-  console.log(obj);
+function createUser(createdUser) {
+  console.log(createdUser);
+
+  // Lägga till saker för användaren
+}
+
+// Skapar nya användare
+class NewUser {
+  constructor(name, password, email) {
+    this.name = name;
+    this.password = password;
+    this.email = email;
+    this.favorites = [];
+  }
+}
+
+// Sparar datan i localstorage
+
+const storedUserJSON = localStorage.getItem("currentUser");
+if (storedUserJSON) {
+  const storedUser = JSON.parse(storedUserJSON);
+  console.log(storedUser);
 }
