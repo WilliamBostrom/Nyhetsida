595eaf69261a4743c3f260f17d5190f0d2796c6a
WilliamBostrom <133894488+WilliamBostrom@users.noreply.github.com>
2024-01-18 11:52:25 +0100

    egna js filer

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 import "./src/css/style.css";
 import "./src/css/general.css";
 import "./src/css/queries.css";
+import "./src/css/login.css";
 
 import "./src/js/script.js";
 import "./src/js/utility/utility.js";
+
+/* Testa kod sidor = tillfälliga */
+import "./src/js/dennis.js";
+import "./src/js/luay.js";
+import "./src/js/simon.js";
+import "./src/js/william.js";
