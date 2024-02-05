c9923ff5bf00946c8c0e95659691b0a1b0eab3c2
Simon-jmk <151679315+Simon-jmk@users.noreply.github.com>
2024-02-05 09:11:37 +0100

    Service worker

---
+++ service-worker.js
@@ -0,0 +1,49 @@
+// Names of the caches used by the service worker
+const CACHE_NAME = 'nyhetsida-chas-news';
+const urlsToCache = [
+  '/',
+  '/src/css/style.css', // Add the correct path to your CSS file
+  'main.js' // Ensure this path matches your structure
+];
+
+// Install a service worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Cache and return requests
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Update a service worker
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
