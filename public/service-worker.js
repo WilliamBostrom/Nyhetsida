7077eb49c2fe3c3c54574ac17617db05b73ddeeb
Simon-jmk <151679315+Simon-jmk@users.noreply.github.com>
2024-02-13 23:27:15 +0100

    Dynamic service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,47 +1,55 @@
-const CACHE_NAME = "nyhetsida-chas-news";
-const urlsToCache = [
-  "index.html",
-  "/assets/index-tBf-FIsm.css",
-  "/assets/index-OpzxNEWx.js",
-  "/CN-logo-box.svg",
-  "/CN-logo-transparent.svg",
-];
+const CACHE_NAME = 'nyhetsida-chas-news-v2';
+const DYNAMIC_CACHE_NAME = 'nyhetsida-dynamic-v2';
 
-self.addEventListener("install", (event) => {
+function shouldCache(response) {
+  return response.ok && (response.type === 'basic' || response.type === 'cors');
+}
+
+self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log("Opened cache");
-      return cache.addAll(urlsToCache);
+      return cache.addAll([
+        'index.html',
+      ]);
     })
   );
+  self.skipWaiting(); 
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      if (response) {
-        return response;
-      }
-      return fetch(event.request).catch(() => {
-        if (event.request.mode === "navigate") {
-          return caches.match("index.html");
-        }
-      });
-    })
-  );
-});
-
-self.addEventListener("activate", (event) => {
-  const cacheWhitelist = [CACHE_NAME];
+self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
         cacheNames.map((cacheName) => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
+          if (cacheName !== CACHE_NAME && cacheName !== DYNAMIC_CACHE_NAME) {
+            return caches.delete(cacheName); 
           }
         })
       );
+    }).then(() => clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  const url = new URL(event.request.url);
+
+  event.respondWith(
+    fetch(event.request).then(fetchResponse => {
+      return caches.open(DYNAMIC_CACHE_NAME).then(cache => {
+        if (shouldCache(fetchResponse)) {
+          cache.put(event.request, fetchResponse.clone());
+        }
+        return fetchResponse;
+      });
+    }).catch(() => {
+      return caches.match(event.request).then(response => {
+        if (response) {
+          return response;
+        }
+        if (event.request.mode === 'navigate') {
+          return caches.match('index.html');
+        }
+      });
     })
   );
 });
